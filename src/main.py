2c483dfc1f799bde7ab7fd593896db047cf9afdd
programmer <info2016frei@gmail.com>
2023-07-13 20:37:04 +0200

    baysian model

--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@
 # Email: leiningc@tf.uni-freiburg.de
 # date: 06.07.2023
 
-import pandas as pd
+import os
 import logging
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-from utils import clean_data, analize_data, split_data, seed_expriement
+from utils import clean_data, analize_data, split_data, seed_expriement, create_scatter_plot
+from utils import baseline, bayesianModel
 
 def main():
     """ Main entry point of the app """
@@ -25,12 +29,31 @@ def main():
     # df_learn_drp =  clean_data(df_learn, policy="drop")
     # df_learn_fill =  clean_data(df_learn, policy="fill")
     df_learn_mean =  clean_data(df_learn, policy="mean")
+    logging.debug(f"df_learn {df_learn.shape}")
+    logging.debug(f"df_learn {df_learn.columns}")
+    
+    bayesianModel(df_learn_mean, df_learn_mean, df_learn_mean)
+    corr_matrix = df_learn_mean.corr()
+    logging.debug(f"corr_matrix {corr_matrix}")
+    corr_with_target = corr_matrix['AKTIV'].sort_values(ascending=False)
+    # Features auswählen, die eine hohe Korrelation mit der Zielvariable haben
+    threathold = 0.05
+    relevant_features = corr_with_target[corr_with_target.abs() > threathold].index.tolist()
+    relevant_features.remove('AKTIV')
+    logging.debug(f"relevant_features {relevant_features}")
+    baseline(df_learn_mean, relevant_features=relevant_features)
+    # Entfernen Sie die Zielvariable selbst aus der Liste
+    corr_matrix_relevant = df_learn_mean[relevant_features].corr()
+    sns.heatmap(corr_matrix_relevant, annot=True)
+    plt.show()
+    import pdb; pdb.set_trace()
     # logging.debug(f"df_learn_drp {df_learn_drp.shape}")
     # logging.debug(f"df_learn_fill {df_learn_fill.shape}")
     logging.debug(f"df_learn_mean {df_learn_mean.shape}")
     # analize_data(df_learn_drp, "AKTIV")
     # analize_data(df = df_learn_fill, label_class="AKTIV", data_name= "flilledWitZero")
-    # analize_data(df = df_learn_mean, label_class="AKTIV", data_name= "flilledWitMean")
+    create_scatter_plot(df_learn_mean, "AKTIV", "flilledWitMean")
+    analize_data(df = df_learn_mean, label_class="AKTIV", data_name= "flilledWitMean")
     # analize_data(df = df_learn_drp, label_class="AKTIV", data_name= "drop")
     
     train_data, valid_data, test_data, train_labels, valid_labels, test_labels = split_data(df_learn_mean, label_class="AKTIV", test_size=0.2, valid_size=0.2, seed=seed)
